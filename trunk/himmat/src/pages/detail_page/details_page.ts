cdf8dfcbbcf67c5268a561c83dc20586d7d00fa0
sacpv04 <sacpv04@gmail.com>
2017-10-02 17:35:20 +0700

    add new branch

--- trunk/himmat/src/pages/detail_page/details_page.ts
+++ trunk/himmat/src/pages/detail_page/details_page.ts
@@ -3,55 +3,56 @@ import { NavParams } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 import { QRCode } from '../qrcode/qrcode';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Patient } from '../services/PatientApi';
 
 @Component({
     templateUrl: 'details_page.html',
   })
   export class DetailsPage {
+    item;
     patient:any;
     maleGender:string;
     femaleGender:string;
-    constructor(public nav: NavController, params: NavParams, private barcodeScanner: BarcodeScanner) {
-      var item = params.data.item;
-      this.patient = new Patient();
-      this.patient.name = item.name;
+    items:any = [];
+    patients:any = [];
+    ohc:any;
+    constructor(public nav: NavController, params: NavParams, private barcodeScanner: BarcodeScanner, private patientAPI: Patient) {
+      this.item = params.data.item;      
+      this.patient = new PatientModel();
+      this.ohc = new OHC();
+      this.patientAPI.getPartients().then(res => {
+        this.items = res;
+        this.items.entry.forEach(element => {                  
+          this.patient.id = element.resource.id;
+          if (element.resource.name) {
+            this.patient.id = element.resource.name[0].family;
+          } else {
+            this.patient.id = "";
+          }          
+          this.patient.gender = element.resource.gender;
+          this.patient.brithday = element.resource.birthDate;
+          this.patient.severity = "";          
+          this.ohc.alleric = "hello"; 
+          this.ohc.bloodtype = ""; 
+          this.ohc.surgery_history = "";
+          this.ohc.mental_illness = "";
+          this.ohc.quick_note = "";
+          this.patient.ohc = this.ohc;
+          console.log(this.ohc.alleric)
+          this.patients.push(this.patient);
+        });
+      });       
     }
+   
     goQRCode() {
       this.nav.push(QRCode);
     }
-    scanQRCode() {
-      var patients = [{
-                      id: "dd50aaf",
-                      name: "Cager Classic",
-                      gender: "Male",
-                      age: "65",
-                      heathcareid: "6b03-4497-b074"
-                      },
-                      {
-                      id: "98c6857e",
-                      name: "Holiday Hoops Challenge",
-                      gender: "Female",
-                      age: "67",
-                      heathcareid: "-b0d1-4295-b89e"
-                      },
-                      {
-                      id: "hh342jjss",
-                      name: "Summer Showdown",
-                      gender: "Male",
-                      age: "68",
-                      heathcareid: "8839-476a-9ba0"
-                      },
-                      {
-                      id: "89e13aa2",
-                      name: "March Madness Tournament",
-                      gender: "Female",
-                      age: "67",
-                      heathcareid: "ba6d-4f55-9cc2"
-                      }];
+
+    scanQRCode() {          
       this.barcodeScanner.scan().then((barcodeData) => {
-        for (var i = 0; i < patients.length; i++) {
-          var id = patients[i].id;
-          var gender = patients[i].gender;
+        for (var i = 0; i < this.patients.length; i++) {
+          var id = this.patients[i].id;
+          var gender = this.patients[i].gender;
           if (gender === "Male") {
             this.maleGender = "checked";
             this.femaleGender = ""
@@ -60,7 +61,7 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
             this.maleGender = "";
           }
           if (id === barcodeData.text) {
-            this.patient = patients[i];
+            this.patient = this.patients[i];
             break; 
           }                   
         }          
@@ -71,10 +72,20 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
     }
 }
 
-class Patient {
+class PatientModel {
   id: string;
   name: string;
   gender: string;
-  age: string;
-  heathcareid: string;
+  brithday: string;
+  severity:string;
+  ohc:any;    
 }
+
+class OHC {
+  alleric:string;
+  bloodtype:string;
+  surgery_history:string;
+  mental_illness:string;
+  quick_note:string;
+}
+
