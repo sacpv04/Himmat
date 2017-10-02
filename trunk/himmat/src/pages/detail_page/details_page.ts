40535db58c83510d614e5a7ebc2c6ea2ad8325c9
sacpv04 <sacpv04@gmail.com>
2017-10-03 00:15:00 +0700

    show patient

--- trunk/himmat/src/pages/detail_page/details_page.ts
+++ trunk/himmat/src/pages/detail_page/details_page.ts
@@ -4,6 +4,7 @@ import { NavController } from 'ionic-angular';
 import { QRCode } from '../qrcode/qrcode';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { Patient } from '../services/PatientApi';
+import { AlertController } from 'ionic-angular';
 
 @Component({
     templateUrl: 'details_page.html',
@@ -14,56 +15,46 @@ import { Patient } from '../services/PatientApi';
     maleGender:string;
     femaleGender:string;
     items:any = [];
-    patients:any = [];
-    constructor(public nav: NavController, params: NavParams, private barcodeScanner: BarcodeScanner, private patientAPI: Patient) {
+    private patients = [];   
+    constructor(public nav: NavController, params: NavParams, private barcodeScanner: BarcodeScanner, private patientAPI: Patient, private alertCtr: AlertController) {
       this.item = params.data.item;      
-      this.patient = new PatientModel();             
+      this.patient = new PatientModel();
+      this.patientAPI.getPartients().then(res => {
+        this.items = res;
+        this.items.entry.forEach(element => {
+          this.patients.push(element.resource);
+          console.log( this.patients);
+        });            
+      });                     
     }
    
     goQRCode() {
       this.nav.push(QRCode);
     }
 
-    scanQRCode() {      
-      this.patientAPI.getPartients().then(res => {
-        this.items = res;
-        this.items.entry.forEach(element => {                  
-          this.patient.id = element.resource.id;
-          if (element.resource.name) {
-            this.patient.name = element.resource.name[0].family;
-          } else {
-            this.patient.name = "";
-          }          
-          this.patient.gender = element.resource.gender;
-          this.patient.brithday = element.resource.birthDate;
-          this.patient.severity = "";         
-          this.patient.alleric = ""; 
-          this.patient.bloodtype = ""; 
-          this.patient.surgery_history = "";
-          this.patient.mental_illness = "";
-          this.patient.quick_note = "";
-          this.patients.push(this.patient);
-        });
-      });
-      this.barcodeScanner.scan().then((barcodeData) => {
+    scanQRCode() {     
+      this.barcodeScanner.scan().then((barcodeData) => {                
         for (var i = 0; i < this.patients.length; i++) {
-          var id = this.patients[i].id;
-          var gender = this.patients[i].gender;
-          if (gender === "Male") {
-            this.maleGender = "checked";
-            this.femaleGender = ""
-          } else {
-            this.femaleGender = "checked";
-            this.maleGender = "";
-          }
+          var id = this.patients[i].id;             
           if (id === barcodeData.text) {
-            this.patient = this.patients[i];
+            this.patient = this.patients[i];            
+            // this.patient.id = this.patients[i].id;
+            this.patient.name = this.patients[i].name[0].family;
+            this.patient.brithday = this.patients[i].birthDate;
+            var gender = this.patients[i].gender;
+            if (gender === "Male") {
+              this.maleGender = "checked";
+              this.femaleGender = ""
+            } else {
+              this.femaleGender = "checked";
+              this.maleGender = "";
+            }           
             break; 
           }                   
-        }          
+        }                
        }, (err) => {
            // An error occurred
-       });
+       });       
     }
 }
 
